d1297b5dcda7751f44ada18ffc0d1dba27620048
aenz <sae@sae.(none)>
2014-05-08 18:17:36 +0200

    changed .gitcommit and added TODO README.txt

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -1,12 +1,12 @@
 public class ExampleTest {
   public static void l() {
-    MissileBattery r = new MissileBattery(6);
-    MissileBattery s = new MissileBattery(10);
+    MissileBattery rawr = new MissileBattery(6);
+    MissileBattery shooty = new MissileBattery(10);
     int i = 20;
     int t = 5;
     t = 5 + i;
-    r.fire(i);
-    s.fire(t);
+    rawr.fire(i);
+    shooty.fire(t);
   }
 }
 
