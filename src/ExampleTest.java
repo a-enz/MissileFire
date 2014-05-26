43646bfcc712a0eaefdede0fb987786278171a85
aenz <andi_enz@msn.com>
2014-05-26 15:30:34 +0200

    merging

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -1,47 +1,15 @@
 public class ExampleTest {
 
-	
-//  public static void l() {
-//	  MissileBattery a = new MissileBattery(5);
-//	  int x = 5;
-//	  int y = 0;
-//	  while(y < x){
-//		  y++;
-//	  }
-//	  a.fire(3);
-//	  a.fire(4);
-//  }
-  
-//  public static void m() {
-//	  MissileBattery a = new MissileBattery(5);
-//	  int x = 5;
-//	  int y = 0;
-//	  while(y < x){
-//		  y++;
-//	  }
-//	  a.fire(4);
-//	  a.fire(4);
-//  }
-
-
-  public static void l() {
-	  int x = 10;
-	  int i = 0;
-	  
-	  MissileBattery a = new MissileBattery(20);
-	  MissileBattery b = new MissileBattery(14);
-	  
-	  while (i < 3){
-		  x++;
-		  i++;
-	  }
-	  MissileBattery r = new MissileBattery(4);
-	  r.fire(i);
-	  a.fire(19);
-	  a.fire(19);
-	  b.fire(x);
-
-  }
-
+	public static void t21_safe() {
+		MissileBattery a = new MissileBattery(4);
+		MissileBattery b = a;
+		int i = 1;
+		int j = 2;
+		while(i + j < 4) {
+			i++;
+			j--;
+		}
+		b.fire(5);
+	}
 }
 
